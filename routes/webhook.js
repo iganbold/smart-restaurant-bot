e9e445de5e147ba842bbc1bad045125878f7a110
iganbold <itgel.ganbold1@gmail.com>
2017-01-11 17:30:36 -0500

    Handle /webhook/ post method

--- routes/webhook.js
+++ routes/webhook.js
@@ -14,4 +14,29 @@ router.get('/', function(req, res, next) {
   }
 });
 
+router.post('/', (req,res)=> {
+  let data = req.body;
+  if(data.object === 'page') {
+    data.entry.forEach((entry)=> {
+      let pageID = entry.id;
+      let timeOfEvent = entry.time;
+
+      entry.messaging.forEach((event) => {
+        if(event.messaging) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
+
 module.exports = router;
